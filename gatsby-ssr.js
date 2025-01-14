464011346ac5c0fd7bf14fa900061d5035445b86
Eldo Macuácua <eldomacuacua@yahoo.com>
2025-01-14 21:46:49 +0530

    Getting Ready to Deploy

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,9 +4,4 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
 
-/**
- * @type {import('gatsby').GatsbySSR['onRenderBody']}
- */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
-}
+
